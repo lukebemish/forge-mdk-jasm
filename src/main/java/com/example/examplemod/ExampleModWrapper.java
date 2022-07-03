78a8459d9e6f7793dae595a7cc35ad2c75210835
Luke Bemish <lukebemish@gmail.com>
2022-07-02 21:13:00 -0500

    Implement rest of mdk

--- src/main/java/com/example/examplemod/ExampleModWrapper.java
+++ src/main/java/com/example/examplemod/ExampleModWrapper.java
@@ -3,9 +3,8 @@ package com.example.examplemod;
 import net.minecraftforge.fml.common.Mod;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleModWrapper.MODID)
+@Mod(ExampleMod.MODID)
 public class ExampleModWrapper {
-    public static final String MODID = "examplemod";
     public ExampleModWrapper() {
         new ExampleMod();
     }
